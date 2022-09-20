eb6adbe71a3ab9e6effe9d4a767503fa03c607e9
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2022-09-20 19:21:10 +0300

    add localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,28 @@ import Filter from "./Filter/Filter";
 export class App extends Component {
 
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',
+  }
+
+  componentDidMount() {
+    if (localStorage.getItem('contacts') !== null) {
+      const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+      if (savedContacts.length !== 0) {
+        this.setState({contacts: savedContacts})
+      }
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem("contacts", JSON.stringify(contacts))
+    }
+  }
+
+  componentWillUnmount() {
+    localStorage.removeItem("contacts");
   }
 
   handleChange = (event) => {
